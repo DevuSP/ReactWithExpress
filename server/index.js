70a6d400452e33cd8f651f721f7ac6885a02efbb
Devashish Singh Parmar <parmardevashish132@gmail.com>
2024-01-22 21:36:04 +0530

    using body parser middleware

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 import express from "express";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path'; 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const app = express();
+app.use(bodyParser.json()); // convert from frontend to backend; (bodyParser.urlencoded({extended: true}))
+app.use(express.cors()); // When you use the cors module in Javascript and provide it the origin argument as http://localhost:8000 it means that the domain localhost can acess your backend resources and get data from it while other domains can't
 const port = 3000;
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
-})
 
+mongoose.connect("");
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port} smoothly.`)
